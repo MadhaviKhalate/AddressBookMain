79338024ed82a34db6db2c4b5239fc933e1321b2
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-05-27 08:58:43 +0530

    [Madhavi] added code to edit contact

--- AddressBookMain/AddressBookMain/Program.cs
+++ AddressBookMain/AddressBookMain/Program.cs
@@ -5,7 +5,8 @@ internal class Program
 
     public static void Main(String[] args)
     {
-        Console.WriteLine("1.Create Contact\n2.Edit Contact\n3.Delete Contact\n4.Display Contacts\n5.Add Multiple Contacts\n6.Add Multiple Address Books\n7.Exit\n");
+        Console.WriteLine("1.Create Contact\n2.Edit Contact\n3.Delete Contact\n4.Display Contacts" +
+            "\n5.Add Multiple Contacts\n6.Add Multiple Address Books\n7.Exit\n");
         Console.WriteLine("Enter your choice:");
         int choice = Convert.ToInt32(Console.ReadLine());
         AddressBookMain.MyPerson p = new AddressBookMain.MyPerson();
@@ -19,10 +20,10 @@ internal class Program
                     AddressBookMain.MyPerson.createContacts();
                     break;
                 case 2:
-                    //AddressBook.MyPerson.editContacts();
+                    AddressBookMain.MyPerson.editContacts();
                     break;
                 case 3:
-                    //AddressBook.MyPerson.removeContact();
+                    //AddressBookMain.MyPerson.removeContact();
                     break;
                 case 4:
                     p.displayContacts();
